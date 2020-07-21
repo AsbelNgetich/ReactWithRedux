ab331c72f625846626dd2de645011d28de87677b
Asbel <asbeln01@yahoo.com>
2020-07-21 12:18:04 -0500

    Converted the videos Project to use hooks.

---
+++ videos-hooks/src/hooks/useVideos.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import youtube from "../apis/youtube";
+
+const KEY = "AIzaSyAD4rYF9L6U-SZ4kqvqpEYSxZq2LC0ReoE";
+
+const useVideos = (defaultSearchTerm) => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    search(defaultSearchTerm);
+  }, [defaultSearchTerm]);
+
+  const search = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+
+    setVideos(response.data.items);
+  };
+  return [videos, search];
+};
+
+export default useVideos;
